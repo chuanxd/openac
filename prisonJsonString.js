3530b12e483acb50fdfa83b57145491faafc489a
chuanXD <timmy5519@gmail.com>
2014-04-29 23:58:03 +0800

    add json format prisonZIP and prisonAddress.
    add json format prisonZIP and prisonAddress.

--- prisonJsonString.js
+++ prisonJsonString.js
@@ -2,199 +2,297 @@ var prisonJsonString = {
 	"prison": [
 		{
 			"prisonName": "法務部矯正署臺北監獄",
-			"prisonWeb": "http://www.tpp.moj.gov.tw"
+			"prisonWeb": "http://www.tpp.moj.gov.tw",
+			"prisonZIP": "33307",
+			"prisonAddress": "桃園縣龜山鄉山鶯路宏德新村2號"
 		},
 		{
 			"prisonName": "法務部矯正署桃園監獄",
-			"prisonWeb": "http://www.typ.moj.gov.tw"
+			"prisonWeb": "http://www.typ.moj.gov.tw",
+			"prisonZIP": "33056",
+			"prisonAddress": "桃園市延壽街158號"
 		},
 		{
 			"prisonName": "法務部矯正署桃園女子監獄",
-			"prisonWeb": "http://www.tyw.moj.gov.tw"
+			"prisonWeb": "http://www.tyw.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署新竹監獄",
-			"prisonWeb": "http://www.scp.moj.gov.tw"
+			"prisonWeb": "http://www.scp.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署臺中監獄",
-			"prisonWeb": "http://www.tcp.moj.gov.tw"
+			"prisonWeb": "http://www.tcp.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署臺中女子監獄",
-			"prisonWeb": "http://www.tcw.moj.gov.tw"
+			"prisonWeb": "http://www.tcw.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署彰化監獄",
-			"prisonWeb": "http://www.chp.moj.gov.tw"
+			"prisonWeb": "http://www.chp.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署雲林監獄",
-			"prisonWeb": "http://www.ulp.moj.gov.tw"
+			"prisonWeb": "http://www.ulp.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署雲林第二監獄",
-			"prisonWeb": "http://www.ulp2.moj.gov.tw"
+			"prisonWeb": "http://www.ulp2.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署嘉義監獄",
-			"prisonWeb": "http://www.cyp.moj.gov.tw"
+			"prisonWeb": "http://www.cyp.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署臺南監獄",
-			"prisonWeb": "http://www.tnp.moj.gov.tw"
+			"prisonWeb": "http://www.tnp.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署明德外役監獄",
-			"prisonWeb": "http://www.mtp.moj.gov.tw"
+			"prisonWeb": "http://www.mtp.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署高雄監獄",
-			"prisonWeb": "http://www.ksp.moj.gov.tw"
+			"prisonWeb": "http://www.ksp.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署高雄第二監獄",
-			"prisonWeb": "http://www.ksd.moj.gov.tw"
+			"prisonWeb": "http://www.ksd.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署高雄女子監獄",
-			"prisonWeb": "http://www.ksw.moj.gov.tw"
+			"prisonWeb": "http://www.ksw.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署屏東監獄",
-			"prisonWeb": "http://www.ptp.moj.gov.tw"
+			"prisonWeb": "http://www.ptp.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署臺東監獄",
-			"prisonWeb": "http://www.ttp.moj.gov.tw"
+			"prisonWeb": "http://www.ttp.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署花蓮監獄",
-			"prisonWeb": "http://www.hlp.moj.gov.tw"
+			"prisonWeb": "http://www.hlp.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署自強外役監獄",
-			"prisonWeb": "http://www.jcp.moj.gov.tw"
+			"prisonWeb": "http://www.jcp.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署宜蘭監獄",
-			"prisonWeb": "http://www.ilp.moj.gov.tw"
+			"prisonWeb": "http://www.ilp.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署基隆監獄",
-			"prisonWeb": "http://www.klp.moj.gov.tw"
+			"prisonWeb": "http://www.klp.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署澎湖監獄",
-			"prisonWeb": "http://www.php.moj.gov.tw"
+			"prisonWeb": "http://www.php.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署綠島監獄",
-			"prisonWeb": "http://www.gip.moj.gov.tw"
+			"prisonWeb": "http://www.gip.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署金門監獄",
-			"prisonWeb": "http://www.kmp.moj.gov.tw"
+			"prisonWeb": "http://www.kmp.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署泰源技能訓練所",
-			"prisonWeb": "http://www.tuv.moj.gov.tw"
+			"prisonWeb": "http://www.tuv.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署東成技能訓練所",
-			"prisonWeb": "http://www.dcv.moj.gov.tw"
+			"prisonWeb": "http://www.dcv.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署岩灣技能訓練所",
-			"prisonWeb": "http://www.ywv.moj.gov.tw"
+			"prisonWeb": "http://www.ywv.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署桃園少年輔育院",
-			"prisonWeb": "http://www.tyr.moj.gov.tw"
+			"prisonWeb": "http://www.tyr.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署彰化少年輔育院",
-			"prisonWeb": "http://www.chr.moj.gov.tw"
+			"prisonWeb": "http://www.chr.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "誠正中學",
-			"prisonWeb": "http://www.ctg.moj.gov.tw"
+			"prisonWeb": "http://www.ctg.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "明陽中學",
-			"prisonWeb": "http://www.myg.moj.gov.tw"
+			"prisonWeb": "http://www.myg.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署臺北看守所",
-			"prisonWeb": "http://www.tpd.moj.gov.tw"
+			"prisonWeb": "http://www.tpd.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署士林看守所",
-			"prisonWeb": "http://www.sld.moj.gov.tw"
+			"prisonWeb": "http://www.sld.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署新竹看守所",
-			"prisonWeb": "http://www.scd.moj.gov.tw"
+			"prisonWeb": "http://www.scd.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署苗栗看守所",
-			"prisonWeb": "http://www.mld.moj.gov.tw"
+			"prisonWeb": "http://www.mld.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署臺中看守所",
-			"prisonWeb": "http://www.tcd.moj.gov.tw"
+			"prisonWeb": "http://www.tcd.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署彰化看守所",
-			"prisonWeb": "http://www.chd.moj.gov.tw"
+			"prisonWeb": "http://www.chd.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署南投看守所",
-			"prisonWeb": "http://www.ntd.moj.gov.tw"
+			"prisonWeb": "http://www.ntd.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署嘉義看守所",
-			"prisonWeb": "http://www.cyd.moj.gov.tw"
+			"prisonWeb": "http://www.cyd.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署臺南看守所",
-			"prisonWeb": "http://www.tnd.moj.gov.tw"
+			"prisonWeb": "http://www.tnd.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署屏東看守所",
-			"prisonWeb": "http://www.ptd.moj.gov.tw"
+			"prisonWeb": "http://www.ptd.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署花蓮看守所",
-			"prisonWeb": "http://www.hld.moj.gov.tw"
+			"prisonWeb": "http://www.hld.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署基隆看守所",
-			"prisonWeb": "http://www.kld.moj.gov.tw"
+			"prisonWeb": "http://www.kld.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署臺北少年觀護所",
-			"prisonWeb": "http://www.tpj.moj.gov.tw"
+			"prisonWeb": "http://www.tpj.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署臺南少年觀護所",
-			"prisonWeb": "http://www.tnj.moj.gov.tw"
+			"prisonWeb": "http://www.tnj.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署臺中戒治所",
-			"prisonWeb": "http://www.tcj.moj.gov.tw"
+			"prisonWeb": "http://www.tcj.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署新店戒治所",
-			"prisonWeb": "http://www.sdb.moj.gov.tw"
+			"prisonWeb": "http://www.sdb.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署臺東戒治所",
-			"prisonWeb": "http://www.ttb.moj.gov.tw"
+			"prisonWeb": "http://www.ttb.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		},
 		{
 			"prisonName": "法務部矯正署高雄戒治所",
-			"prisonWeb": "http://www.ksb.moj.gov.tw"
+			"prisonWeb": "http://www.ksb.moj.gov.tw",
+			"prisonZIP": "",
+			"prisonAddress": ""
 		}
 	]
 }
