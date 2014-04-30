ba236c4a4edc12bbfd762264ba36c1b54560651d
chuanXD <timmy5519@gmail.com>
2014-04-30 19:30:04 +0800

    Add prisonZIP & prisonAddress items.
    Add prisonZIP & prisonAddress items.

--- prisonJsonString.js
+++ prisonJsonString.js
@@ -15,284 +15,284 @@ var prisonJsonString = {
 		{
 			"prisonName": "法務部矯正署桃園女子監獄",
 			"prisonWeb": "http://www.tyw.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "32546",
+			"prisonAddress": "桃園縣龍潭鄉富林村中正路三林段617號"
 		},
 		{
 			"prisonName": "法務部矯正署新竹監獄",
 			"prisonWeb": "http://www.scp.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "30056",
+			"prisonAddress": "新竹市延平路一段108號"
 		},
 		{
 			"prisonName": "法務部矯正署臺中監獄",
 			"prisonWeb": "http://www.tcp.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "40852",
+			"prisonAddress": "台中市南屯區培德路9號"
 		},
 		{
 			"prisonName": "法務部矯正署臺中女子監獄",
 			"prisonWeb": "http://www.tcw.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "40852",
+			"prisonAddress": "台中市南屯區培德路9之3號"
 		},
 		{
 			"prisonName": "法務部矯正署彰化監獄",
 			"prisonWeb": "http://www.chp.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "52601",
+			"prisonAddress": "彰化縣二林鎮二溪路三段240號"
 		},
 		{
 			"prisonName": "法務部矯正署雲林監獄",
 			"prisonWeb": "http://www.ulp.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "63253",
+			"prisonAddress": "雲林縣虎尾鎮仁愛新村1號"
 		},
 		{
 			"prisonName": "法務部矯正署雲林第二監獄",
 			"prisonWeb": "http://www.ulp2.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "63248",
+			"prisonAddress": "雲林縣虎尾鎮建國里建國四村5之18號"
 		},
 		{
 			"prisonName": "法務部矯正署嘉義監獄",
 			"prisonWeb": "http://www.cyp.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "61142",
+			"prisonAddress": "嘉義縣鹿草鄉豐稠村維新新村1號"
 		},
 		{
 			"prisonName": "法務部矯正署臺南監獄",
 			"prisonWeb": "http://www.tnp.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "71150",
+			"prisonAddress": "臺南市歸仁區武東里明德新村1號"
 		},
 		{
 			"prisonName": "法務部矯正署明德外役監獄",
 			"prisonWeb": "http://www.mtp.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "74341",
+			"prisonAddress": "臺南市山上區玉峰里明德山莊1號"
 		},
 		{
 			"prisonName": "法務部矯正署高雄監獄",
 			"prisonWeb": "http://www.ksp.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "83101",
+			"prisonAddress": "高雄市大寮區仁德新村1號"
 		},
 		{
 			"prisonName": "法務部矯正署高雄第二監獄",
 			"prisonWeb": "http://www.ksd.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "824",
+			"prisonAddress": "高雄市燕巢區正德新村一號"
 		},
 		{
 			"prisonName": "法務部矯正署高雄女子監獄",
 			"prisonWeb": "http://www.ksw.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "83156",
+			"prisonAddress": "高雄市大寮區淑德新村1號"
 		},
 		{
 			"prisonName": "法務部矯正署屏東監獄",
 			"prisonWeb": "http://www.ptp.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "91142",
+			"prisonAddress": "屏東縣竹田鄉永豐村永豐路132號"
 		},
 		{
 			"prisonName": "法務部矯正署臺東監獄",
 			"prisonWeb": "http://www.ttp.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "950",
+			"prisonAddress": "台東市廣東路317號"
 		},
 		{
 			"prisonName": "法務部矯正署花蓮監獄",
 			"prisonWeb": "http://www.hlp.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "973",
+			"prisonAddress": "花蓮縣吉安鄉干城村吉安路6段700號"
 		},
 		{
 			"prisonName": "法務部矯正署自強外役監獄",
 			"prisonWeb": "http://www.jcp.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "97642",
+			"prisonAddress": "花蓮縣光復鄉大全村建國路1段1號"
 		},
 		{
 			"prisonName": "法務部矯正署宜蘭監獄",
 			"prisonWeb": "http://www.ilp.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "26646",
+			"prisonAddress": "宜蘭縣三星鄉拱照村三星路3段365巷安農新村1號"
 		},
 		{
 			"prisonName": "法務部矯正署基隆監獄",
 			"prisonWeb": "http://www.klp.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "20143",
+			"prisonAddress": "基隆市東光路199號"
 		},
 		{
 			"prisonName": "法務部矯正署澎湖監獄",
 			"prisonWeb": "http://www.php.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "88592",
+			"prisonAddress": "澎湖縣湖西鄉鼎灣村1之1號"
 		},
 		{
 			"prisonName": "法務部矯正署綠島監獄",
 			"prisonWeb": "http://www.gip.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "95141",
+			"prisonAddress": "臺東縣綠島鄉中寮村192號"
 		},
 		{
 			"prisonName": "法務部矯正署金門監獄",
 			"prisonWeb": "http://www.kmp.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "89142",
+			"prisonAddress": "金門縣金湖鎮復興路1"
 		},
 		{
 			"prisonName": "法務部矯正署泰源技能訓練所",
 			"prisonWeb": "http://www.tuv.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "95992",
+			"prisonAddress": "台東縣東河鄉北源村32號"
 		},
 		{
 			"prisonName": "法務部矯正署東成技能訓練所",
 			"prisonWeb": "http://www.dcv.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "95442",
+			"prisonAddress": "台東縣卑南鄉美農村18鄰班鳩1號"
 		},
 		{
 			"prisonName": "法務部矯正署岩灣技能訓練所",
 			"prisonWeb": "http://www.ywv.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "95058",
+			"prisonAddress": "台東縣台東市興安路2段642號"
 		},
 		{
 			"prisonName": "法務部矯正署桃園少年輔育院",
 			"prisonWeb": "http://www.tyr.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "33051",
+			"prisonAddress": "桃園縣桃園市向善街98號"
 		},
 		{
 			"prisonName": "法務部矯正署彰化少年輔育院",
 			"prisonWeb": "http://www.chr.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "52044",
+			"prisonAddress": "彰化縣田中鎮山腳路五段360巷170號"
 		},
 		{
 			"prisonName": "誠正中學",
 			"prisonWeb": "http://www.ctg.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "30444",
+			"prisonAddress": "新竹縣新豐鄉松柏村11鄰德昌街231號"
 		},
 		{
 			"prisonName": "明陽中學",
 			"prisonWeb": "http://www.myg.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "82444",
+			"prisonAddress": "高雄市燕巢區橫山里正德新村6號"
 		},
 		{
 			"prisonName": "法務部矯正署臺北看守所",
 			"prisonWeb": "http://www.tpd.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "23648",
+			"prisonAddress": "新北市土城區立德路2號"
 		},
 		{
 			"prisonName": "法務部矯正署士林看守所",
 			"prisonWeb": "http://www.sld.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "23656",
+			"prisonAddress": "新北市土城區青雲路33號"
 		},
 		{
 			"prisonName": "法務部矯正署新竹看守所",
 			"prisonWeb": "http://www.scd.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "30056",
+			"prisonAddress": "新竹市延平路一段110號"
 		},
 		{
 			"prisonName": "法務部矯正署苗栗看守所",
 			"prisonWeb": "http://www.mld.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "36063",
+			"prisonAddress": "苗栗縣苗栗市南勢里南勢100號"
 		},
 		{
 			"prisonName": "法務部矯正署臺中看守所",
 			"prisonWeb": "http://www.tcd.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "40852",
+			"prisonAddress": "臺中市南屯區培德路11號"
 		},
 		{
 			"prisonName": "法務部矯正署彰化看守所",
 			"prisonWeb": "http://www.chd.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "510",
+			"prisonAddress": "彰化縣員林鎮法院街73號"
 		},
 		{
 			"prisonName": "法務部矯正署南投看守所",
 			"prisonWeb": "http://www.ntd.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "54048",
+			"prisonAddress": "南投縣南投市嘉和1路1號"
 		},
 		{
 			"prisonName": "法務部矯正署嘉義看守所",
 			"prisonWeb": "http://www.cyd.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "61142",
+			"prisonAddress": "嘉義縣鹿草鄉信義新村1號"
 		},
 		{
 			"prisonName": "法務部矯正署臺南看守所",
 			"prisonWeb": "http://www.tnd.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "71150",
+			"prisonAddress": "台南市歸仁區武東里明德新村2號"
 		},
 		{
 			"prisonName": "法務部矯正署屏東看守所",
 			"prisonWeb": "http://www.ptd.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "91142",
+			"prisonAddress": "屏東縣竹田鄉永豐村永豐路130號"
 		},
 		{
 			"prisonName": "法務部矯正署花蓮看守所",
 			"prisonWeb": "http://www.hld.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "97058",
+			"prisonAddress": "花蓮市美崙日新崗1號"
 		},
 		{
 			"prisonName": "法務部矯正署基隆看守所",
 			"prisonWeb": "http://www.kld.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "20151",
+			"prisonAddress": "基隆市崇法街64號"
 		},
 		{
 			"prisonName": "法務部矯正署臺北少年觀護所",
 			"prisonWeb": "http://www.tpj.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "23654",
+			"prisonAddress": "新北市土城區石門路4號"
 		},
 		{
 			"prisonName": "法務部矯正署臺南少年觀護所",
 			"prisonWeb": "http://www.tnj.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "70263",
+			"prisonAddress": "台南市南區大林路161號"
 		},
 		{
 			"prisonName": "法務部矯正署臺中戒治所",
 			"prisonWeb": "http://www.tcj.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "40852",
+			"prisonAddress": "台中市南屯區培德路3號"
 		},
 		{
 			"prisonName": "法務部矯正署新店戒治所",
 			"prisonWeb": "http://www.sdb.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "23153",
+			"prisonAddress": "新北市新店區莒光路42號"
 		},
 		{
 			"prisonName": "法務部矯正署臺東戒治所",
 			"prisonWeb": "http://www.ttb.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "95542",
+			"prisonAddress": "臺東縣鹿野鄉瑞豐村永嶺路270號"
 		},
 		{
 			"prisonName": "法務部矯正署高雄戒治所",
 			"prisonWeb": "http://www.ksb.moj.gov.tw",
-			"prisonZIP": "",
-			"prisonAddress": ""
+			"prisonZIP": "82409",
+			"prisonAddress": "高雄市燕巢區正德新村5號"
 		}
 	]
 }
